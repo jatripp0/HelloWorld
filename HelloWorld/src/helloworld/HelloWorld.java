5b7725544197b675ff522d47c06a949665efbe5e
jatripp0 <jatripp0@CC270-86279.frostburg.edu>
2016-10-12 09:39:18 -0400

    Steve made me read a file.

--- HelloWorld/src/helloworld/HelloWorld.java
+++ HelloWorld/src/helloworld/HelloWorld.java
@@ -5,6 +5,12 @@
  */
 package helloworld;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 /**
  *
  * @author jatripp0
@@ -15,7 +21,25 @@ public class HelloWorld {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        FileReader f;
+        String line = "";
+        ArrayList<String> lines = new ArrayList<>();
+        try{
+            f = new FileReader(args[0]);
+            BufferedReader reader = new BufferedReader(f);
+            
+            while((line = reader.readLine()) != null){
+                lines.add(line);
+            }
+            
+            for(String s : lines){
+                System.out.printf("  -->%s%n", s);
+            }
+        } catch(FileNotFoundException e){
+            System.err.println("File not found.");
+        } catch(IOException e){
+            System.err.println("There was a problem reading the file.");
+        } 
     }
     
 }
